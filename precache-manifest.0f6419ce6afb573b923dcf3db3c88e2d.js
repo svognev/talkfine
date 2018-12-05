bfaf5cc9c441467db959bd6625d4addad1264cd9
Svetozar Ognev <svognev@yandex.com>
2018-12-05 21:23:56 +0300

    Updates

---
+++ precache-manifest.0f6419ce6afb573b923dcf3db3c88e2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fccfea042e12dc8a141ddd16777c3951",
+    "url": "/talkfine/static/media/redCat.fccfea04.svg"
+  },
+  {
+    "revision": "ec6a85dd6ae1b5f01ee33d216d206da0",
+    "url": "/talkfine/static/media/glowingStar.ec6a85dd.svg"
+  },
+  {
+    "revision": "25c4adbd28a32e138c5f3214a31c763a",
+    "url": "/talkfine/static/media/frog.25c4adbd.svg"
+  },
+  {
+    "revision": "08a997e9994a88275282",
+    "url": "/talkfine/static/js/runtime~main.08a997e9.js"
+  },
+  {
+    "revision": "52d78abfc5e20233194a",
+    "url": "/talkfine/static/js/main.52d78abf.chunk.js"
+  },
+  {
+    "revision": "270a6717f6be2aa57dce",
+    "url": "/talkfine/static/js/1.270a6717.chunk.js"
+  },
+  {
+    "revision": "52d78abfc5e20233194a",
+    "url": "/talkfine/static/css/main.2b273cd2.chunk.css"
+  },
+  {
+    "revision": "e5dec753c29eb915031175211f2621bd",
+    "url": "/talkfine/index.html"
+  }
+];
