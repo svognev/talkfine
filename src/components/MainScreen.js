f0b8eb1865da2e1bc471b03439da3e4114da801d
Svetozar Ognev <svognev@yandex.com>
2018-12-16 13:54:20 +0300

    new flexible design for the main screen

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -8,99 +8,145 @@ import icon06 from "../icon06.svg";
 import icon07 from "../icon07.svg";
 import icon08 from "../icon08.svg";
 import icon09 from "../icon09.svg";
+import icon10 from "../icon10.svg";
+import iconDownwards from "../iconDownwards.svg";
+import iconUpwards from "../iconUpwards.svg"
 
 export class MainScreen extends React.Component {
 
     render() {
         return (
-              <div className="workspace">
-
-                <section className="titleBox">
-                  <h1 className="title">Толк файн</h1>
-                </section>
-              
-                <section className="innerTitleBox">
-                </section>
-              
-                <div className="exercises">
-
-                <section className="topicBox">
-                  <p id="currentTopic">Упражнения по темам:</p>
-                </section>
-                  
-                <div className="grid">
-                  
-                  <div className="a1">
-                    <button className="item" onClick={() => this.props.onClick("exercise01")}>
-                      <img id="icon01" className="itemIcon" src={icon01}></img>
-                    </button>
-                  </div>
-                  <div className="a2">
-                    <button className="item" onClick={() => this.props.onClick("exercise02")}>
-                    <img id="icon02" className="itemIcon" src={icon02}></img>
-                    </button>
-                  </div>
-                  <div className="a3">
-                    <button className="item" onClick={() => this.props.onClick("exercise03")}>
-                    <img id="icon03"  className="itemIcon" src={icon03}></img>
-                    </button>
-                  </div>
-
-                  <div className="b1">
-                    <button className="item" onClick={() => this.props.onClick("exercise04")}>
-                    <img id="icon04"  className="itemIcon" src={icon04}></img>
-                    </button>
-                  </div>
-                  <div className="b2">
-                    <button className="item" onClick={() => this.props.onClick("exercise05")}>
-                    <img id="icon05"  className="itemIcon" src={icon05}></img>
-                    </button>
-                  </div>
-                  <div className="b3">
-                    <button className="item" onClick={() => this.props.onClick("exercise06")}>
-                    <img id="icon06"  className="itemIcon" src={icon06}></img>
-                    </button>
-                  </div>
-
-                  <div className="c1">
-                    <button className="item" id="idiom1Icon" onClick={() => this.props.onClick("exercise07")}>
-                    <img id="icon07"  className="itemIcon" src={icon07}></img>
-                    </button>
-                  </div>
-                  <div className="c2">
-                    <button className="item" id="idiom2Icon" onClick={() => this.props.onClick("exercise08")}>
-                    <img id="icon08"  className="itemIcon" src={icon08}></img>
-                    </button>
-                  </div>
-                  <div className="c3">
-                    <button className="item" id="proverbIcon" onClick={() => this.props.onClick("exercise09")}>
-                    <img id="icon09" className="itemIcon" src={icon09}></img>
-                    </button>
-                  </div>
-                  
-
-                  <div className="a1text"><p className="itemName">Приветствие<br />и прощание</p></div>
-                  <div className="a2text"><p className="itemName">Вводные<br />слова</p></div>
-                  <div className="a3text"><p className="itemName">Согласие и<br />несогласие</p></div>
-                  
-                  <div className="b1text"><p className="itemName">Слова<br />вежливости</p></div>
-                  <div className="b2text"><p className="itemName">Участие в<br />разговоре</p></div>
-                  <div className="b3text"><p className="itemName">Путешествия</p></div>
-                 
-                  <div className="c1text"><p className="itemName">Идиомы<br />Уровень I</p></div>
-                  <div className="c2text"><p className="itemName">Идиомы<br />Уровень II</p></div>
-                  <div className="c3text"><p className="itemName">Пословицы</p></div>
-                
-                </div>
-                
-                <div className="dictionary">
-                
-                    <p id="addOnTitle">Дополнительно:</p>
-                    <p id="dictionaryLink" onClick={() => this.props.onClick("dictionary")}>Словарь популярных фраз</p>
-                </div>
+            <div id="container">
+              <div id="titleBox"></div> 
+              <div id="subtitleBox"></div>  
+              <div id="extraTitleBox"></div>
+              <div id="itemsBox"></div>
+              <div id="glossaryBox"></div>
             
+              <div id="innerTitleBox">
+                <h3 id="logo">Толк файн</h3>
               </div>
-            </div>    
+              <div id="innerSubtitleBox">
+                <p id="subtitle">Упражнения по темам:</p>
+              </div>
+              <div id="innerExtraTitleBox">
+                <p id="extraTitle">Дополнительно:</p>
+              </div>
+  
+              <div id="itemBox01" className="itemBox">
+                <button className="item" onClick={() => this.props.onClick("exercise01")}>
+                  <img id="icon01" className="icons" src={icon01}></img>
+                </button>
+              </div>
+              <div id="subscriptBox01" className="subscriptBox">
+                <p id="subscript01" className="subscript">Приветствие<br />и прощание</p>
+              </div>
+  
+              <div id="itemBox02" className="itemBox">
+                <button className="item" onClick={() => this.props.onClick("exercise02")}>
+                  <img id="icon02" className="icons" src={icon02}></img>
+                </button>
+              </div>
+              <div id="subscriptBox02" className="subscriptBox">
+                <p id="subscript02" className="subscript">Вводные слова</p>
+              </div>
+  
+              <div id="itemBox03" className="itemBox">
+                <button className="item" onClick={() => this.props.onClick("exercise03")}>
+                  <img id="icon03"  className="icons" src={icon03}></img>
+                </button>
+              </div>
+              <div id="subscriptBox03" className="subscriptBox">
+                <p id="subscript03" className="subscript">Согласие и<br />несогласие</p>
+              </div>
+  
+              <div id="itemBox04" className="itemBox">
+                <button className="item" onClick={() => this.props.onClick("exercise04")}>
+                  <img id="icon04"  className="icons" src={icon04}></img>
+                </button>
+              </div>
+              <div id="subscriptBox04" className="subscriptBox">
+                <p id="subscript04" className="subscript">Слова<br />вежливости</p>
+              </div>
+  
+              <div id="itemBox05" className="itemBox">
+                <button className="item" onClick={() => this.props.onClick("exercise05")}>
+                  <img id="icon05"  className="icons" src={icon05}></img>
+                </button>
+              </div>
+              <div id="subscriptBox05" className="subscriptBox">
+                <p id="subscript05" className="subscript">Участие в разговоре</p>
+              </div>
+  
+              <div id="itemBox06" className="itemBox">
+                <button className="item" onClick={() => this.props.onClick("exercise06")}>
+                  <img id="icon06"  className="icons" src={icon06}></img>
+                </button>
+              </div>
+              <div id="subscriptBox06" className="subscriptBox">
+                <p id="subscript06" className="subscript">Путешествия</p>
+              </div>
+  
+              <div id="itemBox07" className="itemBox">
+                <button className="item" id="idiom1Item" onClick={() => this.props.onClick("exercise07")}>
+                  <img id="icon07"  className="icons" src={icon07}></img>
+                </button>
+              </div>
+              <div id="subscriptBox07" className="subscriptBox">
+                <p id="subscript07" className="subscript">Идиомы<br />Уровень I</p>
+              </div>
+  
+              <div id="itemBox08" className="itemBox">
+                <button className="item" id="idiom2Item" onClick={() => this.props.onClick("exercise08")}>
+                  <img id="icon08"  className="icons" src={icon08}></img>
+                </button>
+              </div>
+              <div id="subscriptBox08" className="subscriptBox">
+                <p id="subscript08" className="subscript">Идиомы<br />Уровень II</p>
+              </div>
+  
+              <div id="itemBox09" className="itemBox">
+                <button className="item" id="proverbItem" onClick={() => this.props.onClick("exercise09")}>
+                  <img id="icon09" className="icons" src={icon09}></img>
+                </button>
+              </div>
+              <div id="subscriptBox09" className="subscriptBox">
+                <p id="subscript09" className="subscript">Пословицы</p>
+              </div>
+  
+  
+              <div id="itemBox10" className="itemBox">
+                <button className="item" id="glossaryItem" onClick={() => this.props.onClick("dictionary")}>
+                   <img id="icon10" className="icons" src={icon10}></img>
+                </button>
+              </div>
+              <div id="subscriptBox10" className="subscriptBox">
+                  <p id="subscript10" className="subscript">Словарь<br />выражений</p>
+              </div>
+
+              <div id="levelBox">
+                <button className="idiomsLevel" id="levelUp" onClick={() => {
+                  document.getElementById("levelUp").style.display = "none";
+                  document.getElementById("itemBox07").style.zIndex = 3;
+                  document.getElementById("subscriptBox07").style.zIndex = 3;
+                  document.getElementById("levelDown").style.display = "flex";
+                  document.getElementById("itemBox08").style.zIndex = 4;
+                  document.getElementById("subscriptBox08").style.zIndex = 4;
+                }}>
+                  <img className="iconLevel" id="iconLevelUp" src={iconUpwards}></img>
+                </button>
+                <button className="idiomsLevel" id="levelDown" onClick={() => {
+                  document.getElementById("levelDown").style.display = "none";
+                  document.getElementById("itemBox08").style.zIndex = 3;
+                  document.getElementById("subscriptBox08").style.zIndex = 3;
+                  document.getElementById("levelUp").style.display = "flex";
+                  document.getElementById("itemBox07").style.zIndex = 4;
+                  document.getElementById("subscriptBox07").style.zIndex = 4;
+                }}>
+                  <img className="iconLevel" id="iconLevelDown" src={iconDownwards}></img>
+                </button>
+              </div>
+           </div>
         );
     }
 }
